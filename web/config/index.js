1afeabcb75f60c33d17ace5f904c19e1b6d1eb14
Fredrik Löwenhamn <lowet84@gmail.com>
2018-02-08 16:27:45 +0100

    Added proxy table

--- web/config/index.js
+++ web/config/index.js
@@ -10,7 +10,12 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+        target: 'https://portal.fredriklowenhamn.com',
+        changeOrigin: true
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
