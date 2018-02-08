72f187527ef957a015f1f890a3267104620d30ee
root <root@Ubuntu.asdu3tqgoxnuzcsg1dc3czbidb.fx.internal.cloudapp.net>
2018-02-08 15:21:04 +0000

    For the sake of my sanity, just ignore this...

--- server/server.mjs
+++ server/server.mjs
@@ -7,8 +7,8 @@ app.get('/api/:target', asyncHandler(async (req, res, next) => {
     let url = 'http://'+req.params.target+':8080/api'
     console.log(url)
     let data = await axios.get(url)
-    console.log(data)
-    res.send(data.status)
+    console.log(data.data)
+    res.send(data.data)
 }))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
