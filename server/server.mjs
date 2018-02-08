7b76aedb5924d9469452035f7f3cd7889acef84e
root <root@Ubuntu.asdu3tqgoxnuzcsg1dc3czbidb.fx.internal.cloudapp.net>
2018-02-08 15:11:20 +0000

    Not sure why

--- server/server.mjs
+++ server/server.mjs
@@ -4,7 +4,7 @@ import axios from 'axios'
 const app = express()
 
 app.get('/api/:target', asyncHandler(async (req, res, next) => {
-    let url = req.params.target+':8080/api'
+    let url = 'http://'+req.params.target+':8080/api'
     console.log(url)
     let data = await axios.get(url)
     res.send(data)
