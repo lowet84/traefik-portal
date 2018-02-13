5c6034a58cef1880f31e704a6e11c1246bc87817
Fredrik Löwenhamn <lowet84@gmail.com>
2018-02-13 12:36:46 +0100

    Added image path env variable

--- server/server.js
+++ server/server.js
@@ -9,6 +9,9 @@ app.use(express.json());
 var hosts = process.env.TRAEFIK === undefined
     ? "traefik,public"
     : process.env.TRAEFIK
+var imagePath = process.env.IMAGE_PATH === undefined
+? "/data"
+: process.env.IMAGE_PATH
 hosts = hosts.split(',')
 app.get('/api/', asyncHandler(async (req, res, next) => {
     var data = []
@@ -18,17 +21,17 @@ app.get('/api/', asyncHandler(async (req, res, next) => {
         let result = await axios.get(url)
         data.push(result.data)
     }
-    
+
     res.send(await prepareData(data));
 }))
 
 app.get('/dummy/', asyncHandler(async (req, res, next) => {
-    let file = JSON.parse(await fs.readFileSync('./traefik-test.json', 'utf8'))
+    let file = JSON.parse(await fs.readFileSync('./server/traefik-test.json', 'utf8'))
     res.send(await prepareData(file))
 }))
 
 var prepareData = async function (data) {
-    let images = JSON.parse(await fs.readFileSync('./images.json', 'utf8'))
+    let images = JSON.parse(await fs.readFileSync(`${imagePath}/images.json`, 'utf8'))
     var ret = []
     console.log(data)
     data.forEach(d => Object.values(Object.values(d)[0].frontends).forEach(e => ret.push(e)))
@@ -46,7 +49,7 @@ var prepareData = async function (data) {
 
 app.post('/download', asyncHandler(async (req, res) => {
     console.log(req.body)
-    let images = JSON.parse(await fs.readFileSync('./images.json', 'utf8'))
+    let images = JSON.parse(await fs.readFileSync(`${imagePath}/images.json`, 'utf8'))
     images[req.body.id] = req.body.url
     await fs.writeFileSync('./images.json', JSON.stringify(images))
 }))
